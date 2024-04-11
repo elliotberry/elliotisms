51ca9c5910af4fb5bf56f6b64a695f4c4c54c91e
elliot b  =^._.^= ∫ <elliot@email.gd>
2024-04-10 23:13:05 -0400

    fix obvious error

--- src/createFolderIfItDoesNotExist.js
+++ src/createFolderIfItDoesNotExist.js
@@ -1,9 +1,11 @@
 import exists from './exists.js';
 import fs from "node:fs/promises";
+import path from "node:path";
 
-const createFolderIfItDoesNotExist = (path) => {
-  if (!exists(path)) {
-    fs.mkdirSync(path);
+const createFolderIfItDoesNotExist = async (directoryPath) => {
+  if (await !exists(directoryPath)) {
+    await fs.mkdir(directoryPath);
   }
+  return path.resolve(directoryPath);
 }
 export default createFolderIfItDoesNotExist;
