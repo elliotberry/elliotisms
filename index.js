9826da427428b166ab834008ba769432662ecc6a
elliot b  =^._.^= ∫ <elliot@email.gd>
2024-04-14 23:47:03 -0400

    adding my execa

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import exists from './src/exists.js';
-import createFolderIfItDoesNotExist from './src/createFolderIfItDoesNotExist.js';
-import {formatDateToNice, getDateTime} from './src/nice-date.js';
-import theEpoch from './src/the-epoch.js';
-export {exists, createFolderIfItDoesNotExist, formatDateToNice, getDateTime, theEpoch};
+import exists from './lib/exists.js';
+import createFolderIfItDoesNotExist from './lib/createFolderIfItDoesNotExist.js';
+import {formatDateToNice, getDateTime} from './lib/nice-date.js';
+import theEpoch from './lib/the-epoch.js';
+import exec from './lib/execa.js';
+export {exists, createFolderIfItDoesNotExist, formatDateToNice, getDateTime, theEpoch, exec};
