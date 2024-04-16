c5f5675209ab2aa174b22ff888089814b31a721f
elliot b  =^._.^= ∫ <elliot@email.gd>
2024-04-16 19:07:53 -0400

    📝 (index.js): rename execa.js to exec.js for consistency and clarity 📝 (exec.js): refactor execa function to use node:child_process exec for better error handling and output capturing ⬆️ (package.json): bump up version to 0.0.9 and update supported node version to >=20

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import exists from './lib/exists.js';
 import createFolderIfItDoesNotExist from './lib/createFolderIfItDoesNotExist.js';
 import {formatDateToNice, getDateTime} from './lib/nice-date.js';
 import theEpoch from './lib/the-epoch.js';
-import exec from './lib/execa.js';
+import exec from './lib/exec.js';
 export {exists, createFolderIfItDoesNotExist, formatDateToNice, getDateTime, theEpoch, exec};
