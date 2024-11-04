ec65412d6042be35b684fd18cdb8399883cbc959
ɛʟʟɨօȶ b  =^._.^= ∫ <elliot@email.gd>
2024-11-04 10:43:53 -0500

    🔧 (eslint.config.js): Update eslint plugins and configurations to latest versions for improved code quality and security ♻️ (xxhash.js): Refactor hashIt function to hashFile and add new hashString function for better code organization and readability ⬆️ (package.json): Upgrade eslint and related plugins to latest versions for better linting and code quality assurance

--- eslint.config.js
+++ eslint.config.js
@@ -1,53 +1,67 @@
-import js from "@eslint/js"
-import noSecrets from "eslint-plugin-no-secrets"
-import packageJson from "eslint-plugin-package-json/configs/recommended"
-//import perfectionist from 'eslint-plugin-perfectionist'
-import pluginSecurity from "eslint-plugin-security"
-import is from "eslint-plugin-simple-import-sort"
-import sonarjs from "eslint-plugin-sonarjs"
-import eslintPluginUnicorn from "eslint-plugin-unicorn"
-import importPlugin from "eslint-plugin-import"
+import noSecrets from 'eslint-plugin-no-secrets'
+import perfectionist from 'eslint-plugin-perfectionist'
+import pluginSecurity from 'eslint-plugin-security'
+import is from 'eslint-plugin-simple-import-sort'
+import sonarjs from "eslint-plugin-sonarjs";
+import eslintPluginUnicorn from 'eslint-plugin-unicorn'
+import nodePlugin from 'eslint-plugin-n'; 
+import importPlugin from 'eslint-plugin-import';
+import pluginPromise from 'eslint-plugin-promise'
 export default [
-  eslintPluginUnicorn.configs["flat/recommended"],
+    eslintPluginUnicorn.configs['flat/recommended'],
+    pluginSecurity.configs.recommended,
+    sonarjs.configs.recommended,
+    nodePlugin.configs["flat/recommended-script"],
+    importPlugin.flatConfigs.recommended,
+    pluginPromise.configs['flat/recommended'],
+    {
+        files: ['**/*.js'],
+        languageOptions: {
+            ecmaVersion: 2024,
+            globals: {
+                clearTimeout: 'readonly',
+                console: 'readonly',
+                fetch: 'readonly',
+                process: 'readonly',
+                setTimeout: 'readonly',
+                timeOut: 'readonly',
+            },
+            sourceType: 'module',
+        },
+        plugins: {
+            'no-secrets': noSecrets,
+            'simple-import-sort': is,
+            perfectionist
+        },
 
-  js.configs.recommended,
-  pluginSecurity.configs.recommended,
-  sonarjs.configs.recommended,
-  packageJson,
-  importPlugin.flatConfigs.recommended,
-  {
-    files: ["**/*.js"],
-    languageOptions: {
-      ecmaVersion: 2024,
-      globals: {
-        clearTimeout: "readonly",
-        console: "readonly",
-        fetch: "readonly",
-        process: "readonly",
-        setTimeout: "readonly",
-        timeOut: "readonly"
-      },
-      sourceType: "module"
+        rules: {
+            
+            'no-console': 'off',
+            'no-secrets/no-secrets': 'error',
+            'no-unused-vars': 'warn',
+            'perfectionist/sort-array-includes': 'warn',
+            'perfectionist/sort-astro-attributes': 'warn',
+            'perfectionist/sort-classes': 'warn',
+            'perfectionist/sort-enums': 'warn',
+            'perfectionist/sort-exports': 'warn',
+            'perfectionist/sort-imports': 'warn',
+            'perfectionist/sort-interfaces': 'warn',
+            'perfectionist/sort-jsx-props': 'warn',
+            'perfectionist/sort-maps': 'warn',
+            'perfectionist/sort-named-exports': 'warn',
+            'perfectionist/sort-named-imports': 'warn',
+            'perfectionist/sort-object-types': 'warn',
+            'perfectionist/sort-objects': 'warn',
+            'simple-import-sort/exports': 'warn',
+            'simple-import-sort/imports': 'warn',
+            'unicorn/consistent-function-scoping': 'warn',
+            'unicorn/no-array-callback-reference': 'warn',
+            'unicorn/no-null': 'warn',
+            'unicorn/no-process-exit': 'off',
+            'unicorn/prefer-switch': 'off',
+            'unicorn/prefer-top-level-await': 'off',
+            'unicorn/prevent-abbreviations': 'warn',
+            "security/detect-non-literal-fs-filename": "off"
+        },
     },
-    plugins: {
-      "no-secrets": noSecrets,
-      "simple-import-sort": is
-    },
-
-    rules: {
-      "no-console": "off",
-      "no-secrets/no-secrets": "error",
-      "no-unused-vars": "warn",
-
-      "simple-import-sort/exports": "warn",
-      "simple-import-sort/imports": "warn",
-      "unicorn/consistent-function-scoping": "warn",
-      "unicorn/no-array-callback-reference": "warn",
-      "unicorn/no-null": "warn",
-      "unicorn/no-process-exit": "off",
-      "unicorn/prefer-switch": "off",
-      "unicorn/prefer-top-level-await": "off",
-      "unicorn/prevent-abbreviations": "warn"
-    }
-  }
 ]
